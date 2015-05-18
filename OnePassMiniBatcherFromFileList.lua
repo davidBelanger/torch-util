9692ddb09435e540851bcfb0e08ea496124ab72f
David Belanger <david.b.belanger@gmail.com>
2015-05-18 17:23:19 -0400

    updates

--- OnePassMiniBatcherFromFileList.lua
+++ OnePassMiniBatcherFromFileList.lua
@@ -1,26 +1,29 @@
 local OnePassMiniBatcherFromFileList = torch.class('OnePassMiniBatcherFromFileList')
 
-function OnePassMiniBatcherFromFileList:__init(fileList,batchSize,cuda,preprocess,debugMode)
+function OnePassMiniBatcherFromFileList:__init(fileList,batchSize,useCuda,preprocess,debugMode)
 	self.debugMode = debugMode or false
-	self.batcher_test = MinibatcherFromFileList(params.testList,params.minibatch,useCuda)
-	batcher_test.debugMode = debugMode
+	self.batcher = MinibatcherFromFileList(fileList,batchSize,useCuda)
+	self.batcher.debugMode = debugMode
+	self.preprocess = preprocess
 	self.debugMode = debugMode
-	self.all_batches = batcher:getAllBatches()
+	if(not self.debugMode) then
+		self.all_batches = self.batcher:getAllBatches()
+	end
 	self.tbi = 0
 	self.called = false
 end
 
 function OnePassMiniBatcherFromFileList:getBatch()
 	if(self.debugMode) then
-			local lab,feats,num = batcher_test:getBatch()
+			local lab,feats,num = self.batcher:getBatch()
 			if(not self.called) then
 				self.called = true
 				return self.preprocess(lab,feats,num)
 			end
 	else
 		self.tbi = self.tbi + 1
-		if(self.tbi <= #all_test_batches) then	
-			local lab,feats,num = unpack(all_test_batches[self.tbi])
+		if(self.tbi <= #self.all_batches) then	
+			local lab,feats,num = unpack(self.all_batches[self.tbi])
 			return self.preprocess(lab,feats,num)
 		end
 	end
