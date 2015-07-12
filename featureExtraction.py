4a285ce5eb3489c8aa9fb1b5b651007be99cd287
David Belanger <david.b.belanger@gmail.com>
2015-07-12 23:46:22 +0200

    updates

--- featureExtraction.py
+++ featureExtraction.py
@@ -1,6 +1,8 @@
 import argparse
 import fileinput
-
+from collections import defaultdict
+import operator
+import json
 
 
 #you may want to change this
@@ -23,7 +25,7 @@ def main():
 	args = parser.parse_args()
 
 	makeDomain = args.makeDomain
-	featureTemplates = FeatureTemplates(args.tokenFeatures,args.featureTemplates)
+	featureTemplates = FeatureTemplates(args.tokenFeatures,args.featureTemplates,args.featureCountThreshold)
 	if(not makeDomain):
 		featureTemplates.loadDomain(args.domain)
 
@@ -32,12 +34,11 @@ def main():
 		label = fields[0]
 		text = fields[1].rstrip()
 		toks = tokenize(text)
-		stringFeatures = featureTemplates.extractFeatures(toks)
+		print(toks)
+		stringFeatures = map(lambda tok: featureTemplates.extractFeatures(tok), toks)
 		if(not makeDomain):
-			intFeatures = featureTemplates.convertToInt(stringFeatures)
+			intFeatures = map(lambda tokStringFeats: featureTemplates.convertToInt(tokStringFeats), stringFeatures)
 			print "{0}\t{1}".format(label,featureTemplates.convertFeaturesForPrinting(intFeatures))
-		else:
-			featureDomains.increment(stringFeatures)
 
 	if(makeDomain):
 		print("finished processing text. Now constructing domains")
@@ -47,31 +48,103 @@ def main():
 
 
 
+class FeatureTemplates:
 
-#todo: make a feature templates object 
-#todo: make a feature template object
-#todo: make sure it 
-API: 
-loadDomains(outBase)
-writeDomains(outBase)
-increment(stringFeatures)
-constructDomains()
-convertToInt(stringFeatures)
-extractFeatures(tokenString)
+	candidateTemplates = []
+	featureTemplates = []
+	def __init__(self,useTokenFeatures,featureTemplateNames,featureCountThreshold):
+		self.useFeats = useTokenFeatures
+		if(self.useFeats):
+			self.featureTemplateNames = featureTemplateNames.split(",")
+		self.featureCountThreshold = featureCountThreshold
+		self.featureTemplates.append(TokenString())
 
-#this converts the mapped features so that they can be written out
-todo: this should be a method on featureTemplates
-def convertFeaturesForPrinting(sentenceFeatures):
-	if(not tokenFeatures):
-		return " ".join(map(lambda x: x[0],sentenceFeatures)
-	else:
-		return " ".join(map(lambda x: ",".join(x),sentenceFeatures)
+		#todo: need to use the featureTemplateNames variable
 
+		for d in self.featureTemplates:
+			d.buildCounts = True
+
+	def convertToInt(self,tokStringFeats):
+		return map(lambda x: x[1].convertToInt(x[0]),zip(tokStringFeats,featureTemplates))
 
 
+	def loadDomains(self,domainFileBase): 
+		for template in self.featureTemplates:
+			template.loadDomain(domainFileBase + "." + template.name)
+
+
+	def writeDomains(self,domainFileBase): 
+		for template in self.featureTemplates:
+			template.writeDomain(domainFileBase + "." + template.name)
+
+	def constructDomains(self):
+		for d in self.featureTemplates:
+			d.constructDomain(self.featureCountThreshold)
+			d.buildCounts = false
+
+	def extractFeatures(self,tokenString):
+		normalizedString = tokenString #todo: change
+		return map(lambda x: x.extractFeature(normalizedString),self.featureTemplates)
+
+
+	def convertFeaturesForPrinting(self,sentenceFeatures):
+		if(not useFeats):
+			return " ".join(map(lambda x: x[0],sentenceFeatures))
+		else:
+			return " ".join(map(lambda x: ",".join(x),sentenceFeatures))
+
+
+class FeatureTemplate:
+	def __init__(self):
+		self.buildCounts = True
+		self.counts = defaultdict(int)
+		self.domain = None
+
+
+	def extractFeature(self,normalizedString):
+		feat = self.featureFunction(normalizedString)
+		print feat
+		if(self.buildCounts):
+			self.counts[feat]+= 1 
+		return feat
 
 
 
+	def writeDomain(self,file):
+		data = {
+			name:self.name,
+			domain : self.domain
+		}
+		with open(file, 'w') as outfile:
+   			json.dump(data, outfile)
+
+
+
+	def constructDomain(self,featureCountThreshold):
+		self.domain =  sorted({k: v for k, v in self.counts.iteritems() if v > featureCountThreshold},key=lambda x: x[1])
+		self.domain[defaultValue] = len(self.domain) + 1
+
+
+    def convertToInt(self,feat):
+    	if(feat in self.domain):
+    		return self.domain[feat]
+    	else:
+    		return self.domain[defaultValue]
+
+
+
+	def loadDomain(self,file):
+		with open(file, 'r') as datfile:
+   			data = json.load(datfile)
+   			assert data.name == self.name
+    		self.domain = data.domain
+
+
+##these are the actual feature templates
+class TokenString(FeatureTemplate):
+	name = 'tokenString'
+	def featureFunction(self,normalizedString):
+		return normalizedString
 
 
 if __name__ == "__main__":
