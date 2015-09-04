5880c2e6b9c92174339239bffe24412e733116d7
David Belanger <david.b.belanger@gmail.com>
2015-09-04 10:19:49 -0400

    Revert "preprocessing up front"
    This reverts commit 0251201bedff9913f14f7e140262efd7a2816e92.

--- MinibatcherFromFileList.lua
+++ MinibatcherFromFileList.lua
@@ -7,10 +7,11 @@ function MinibatcherFromFileList:__init(fileList,batchSize,cuda,preprocess)
 	self.preprocess = preprocess
 	self.batches = {}
 	local counts = {}
+	self.debugMode = false
 	print(string.format('reading file list from %s',fileList))
 
 	for file in io.lines(fileList) do
-		local batch  = MinibatcherFromFile(file,batchSize,cuda,preprocess)
+		local batch  = MinibatcherFromFile(file,batchSize,cuda)
 		table.insert(counts,batch.numRows)
 		table.insert(self.batches,batch)
 	end
@@ -23,17 +24,35 @@ function MinibatcherFromFileList:__init(fileList,batchSize,cuda,preprocess)
 end
 
 function  MinibatcherFromFileList:getBatch()
+	if(self.debugMode) then
+		if(self.called) then
+			return self.debug, self.debug2, self.debug3
+		else
+			local idx = torch.multinomial(self.weights,1)
+			self.debug, self.debug2, self.debug3 = preprocess(self.batches[idx[1]]:getBatch())
+			self.called = true
+
+			return self.debug,self.debug2, self.debug3
+		end	
+	end
+
 	local idx = torch.multinomial(self.weights,1)
-	return self.batches[idx[1]]:getBatch()
+	return self.preprocess(self.batches[idx[1]]:getBatch())
 end
 
 function MinibatcherFromFileList:getAllBatches()
 	local t = {}
-	for _,b in ipairs(self.batches) do
-		while(true) do
-			local lab,data,unpadded_len = b:getBatchSequential()
-			if(data == nil) then break end
-			table.insert(t,{lab,data,unpadded_len})
+	if(self.debugMode) then 
+		local x,y,z = self.preprocess(unpack(self.batches[1]:getBatch()))
+		table.insert(t,{x,y,z})
+	else	
+		for _,b in ipairs(self.batches) do
+			while(true) do
+				local lab,data,unpadded_len = b:getBatchSequential()
+				if(data == nil) then break end
+				local a,b,c = self.preprocess(lab,data,unpadded_len)
+				table.insert(t,{a,b,c})
+			end
 		end
 	end
 	return t
