07830255c0442861f54364d53b626e93fae83de9
David Belanger <david.b.belanger@gmail.com>
2015-11-20 11:24:53 -0500

    I think MapReduce.lua works. Need to make sure accGradParameters is ok (only issue if you don't call from backward)

--- MapReduce.lua
+++ MapReduce.lua
@@ -2,8 +2,10 @@ local MapReduce, parent = torch.class('nn.MapReduce', 'nn.Container')
 
 
 function MapReduce:__init(mapper,reducer)
+	parent:__init()
 	self.mapper = mapper
 	self.reducer = reducer
+
 	table.insert(self.modules,mapper)
 	table.insert(self.modules,reducer)
 
@@ -12,14 +14,13 @@ end
 function MapReduce:updateOutput(input)
 	--first, reshape the data by pulling the second dimension into the first
 	self.inputSize = input:size()
-	local numPerExample = inputSizes[2]
-	local minibatchSize = inputSizes[1]
-	self.sizes = self.sizes or torch.LongStorage(self.inputSize:dim() -1)
+	local numPerExample = self.inputSize[2]
+	local minibatchSize = self.inputSize[1]
+	self.sizes = self.sizes or torch.LongStorage(self.inputSize:size() -1)
 	self.sizes[1] = minibatchSize*numPerExample
-	for i = 2,self.inputSize:dim()
-		self.sizes[i-1] = self.inputSize[i]
+	for i = 2,self.sizes:size() do
+		self.sizes[i] = self.inputSize[i+1]
 	end
-
 
 	self.reshapedInput = input:reshape(self.sizes)
 	self.mapped = self.mapper:forward(self.reshapedInput)
@@ -28,8 +29,9 @@ function MapReduce:updateOutput(input)
 	self.sizes2 = self.sizes2 or torch.LongStorage(self.mapped:dim() + 1)
 	self.sizes2[1] = minibatchSize
 	self.sizes2[2] = numPerExample
-	for i = 2,self.mapped:dim()
-		self.sizes[i+1] = self.mapped:size(i)
+
+	for i = 2,self.mapped:dim() do
+		self.sizes2[i+1] = self.mapped:size(i)
 	end
 
 	self.mappedAndReshaped = self.mapped:reshape(self.sizes2)
@@ -60,12 +62,13 @@ end
 
 
 function MapReduce:genericBackward(operator,input, gradOutput)
-
+	local db = self.reducer:forward(self.mappedAndReshaped)
+	self.reducer:backward(self.mappedAndReshaped,db:clone():fill(1.0))
 	local reducerGrad = operator(self.reducer,self.mappedAndReshaped,gradOutput)
-	local reshapedReducerGrad = grad1:reshape(self.sizes3)
+	local reshapedReducerGrad = reducerGrad:reshape(self.sizes3)
 
 	local mapperGrad = operator(self.mapper,self.reshapedInput,reshapedReducerGrad) 
 
-	self.gradInput = mapperGrad:reshape(self.inputSize)
+	self.gradInput = (mapperGrad:dim() > 0) and mapperGrad:reshape(self.inputSize) or nil --some modules return nil from backwards, such as the lookup table
 	return self.gradInput
 end
